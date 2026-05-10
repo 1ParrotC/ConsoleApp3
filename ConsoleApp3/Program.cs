b34ab73006442669b97342be78ac8342429a1441
ParrotC <pablocrespobles@gmail.com>
2026-05-10 12:04:03 +0200

    Ejercicio1

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using ConsoleApp3;
+
+var libro = new Libro("Cien años de soledad", "Gabriel García Márquez", 1967, true);
+Console.WriteLine(libro.ToString());
+Console.WriteLine($"Disponible: {(libro.Disponible ? "Sí" : "No")}");
